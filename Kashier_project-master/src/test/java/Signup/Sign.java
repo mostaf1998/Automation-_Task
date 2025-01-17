0792276c033799ce9c10478dea59f9c540cbab60
mostaf1998 <me7666011@gmail.com>
2025-01-18 01:30:18 +0200

    oen

--- Kashier_project-master/src/test/java/Signup/Sign.java
+++ Kashier_project-master/src/test/java/Signup/Sign.java
@@ -41,6 +41,26 @@ public class Sign {
 
 
     }
+    @Test
+    public void signup1() {
+        page.name(driver).sendKeys("Eng Mostafa Mostafa");
+        page.store(driver).sendKeys("Amazone");
+        page.register(driver).click();
+        page.phone(driver).sendKeys("01229902545");
+        page.email(driver).sendKeys("mostafaelsayed3030@outlook.com");
+        page.password1(driver).sendKeys("Mostafa1998199888@");
+        page.password2(driver).sendKeys("Mostafa1998199888@");
+        page.signup(driver).click();
+
+
+        //Assertion1
+        Assert.assertEquals(driver.getCurrentUrl(), "https://merchant.kashier.io/signup", "True");
+
+
+    }
+
+
+
 
 
 
