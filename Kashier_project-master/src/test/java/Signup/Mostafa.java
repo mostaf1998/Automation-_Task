90fe1c6b11bc135a477a831996ee04040f8607f2
mostaf1998 <me7666011@gmail.com>
2025-01-18 03:39:17 +0200

    update Mostafa Cjass

---
+++ Kashier_project-master/src/test/java/Signup/Mostafa.java
@@ -0,0 +1,30 @@
+package Signup;
+
+import Pages.Page_signup;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Mostafa {
+    WebDriver driver = null;
+    Page_signup page = new Page_signup();
+
+    @Test
+    public void signup1() {
+        page.name(driver).sendKeys("Eng Mostafa Mostafa");
+        page.store(driver).sendKeys("Amazone");
+        page.register(driver).click();
+        page.phone(driver).sendKeys("01229902545");
+        page.email(driver).sendKeys("mostafaelsayed3030@outlook.com");
+        page.password1(driver).sendKeys("Mostafa1998199888@");
+        page.password2(driver).sendKeys("Mostafa1998199888@");
+        page.signup(driver).click();
+
+
+        //Assertion1
+        Assert.assertEquals(driver.getCurrentUrl(), "https://merchant.kashier.io/signup", "True");
+
+
+    }
+
+}
